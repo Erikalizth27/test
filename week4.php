aad8f8e9aa44fa55714677aabf32d3e402d4919f
erika l reyes <elr22@afsconnect1.njit.edu>
2017-10-07 20:48:03 -0400

    change the name

--- week4.php
+++ week4.php
@@ -1,20 +1,9 @@
-<!DOCTYPE html>
-<html>
-<body>
-
 <?php
-
-
-$date = date('Y-m-d');
-$tar = date('Y/m/d');
-$year = array('2012','396','300','2000','1100','1089');
-
-echo 'The value of $date:' . $date ."<br>";
-echo 'The value of $tar: '. $tar."<br>";
-echo 'The value of $year: ';
-print_r($year)."<br>";
-
-?> 
-
-</body>
-</html>
+	$date =  date('Y-m-d', time());
+	echo "The value of \$date: ".$date."<br>";
+	$tar = "2017/05/24";
+	echo "The value of \$tar: ".$tar."<br>";
+	$year = array("2012", "396", "300","2000", "1100", "1089");
+	echo "The value of \$year: ";
+	print_r($year);
+?>
